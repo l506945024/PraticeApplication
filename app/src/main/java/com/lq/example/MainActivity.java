c2d69ab52c18ead4f8085238a561d00f4a67e310
mick <l506945024@163.com>
2018-11-22 10:46:17 +0800

    jenkins测试

--- app/src/main/java/com/lq/example/MainActivity.java
+++ app/src/main/java/com/lq/example/MainActivity.java
@@ -3,15 +3,18 @@ package com.lq.example;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.lq.example.livedata.TestLiveDataActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         startActivity(new Intent(this,TestLiveDataActivity.class));
+
+        Log.d(TAG, "onCreate: "+BuildConfig.serviceIp);
     }
 }
